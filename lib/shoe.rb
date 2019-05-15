806bdc4b98891f2f3b619d556424f45c608e32ef
Kemunto Maragia <kmaragia@email.wm.edu>
2019-05-15 14:48:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,3 +1,12 @@
 # Make your shoe class here!
 class Shoe
-  def initialize()
+  def initialize(brand,color,size,material,condition)
+    @brand = brand
+    @color = color
+    @size = size
+    @material = material
+    @condition = condition
+  end
+  
+  
+end
